8f2893906a0b52c1caf1f3fbca2f1950fd6c372b
Diogo0602x <diogoteixeira0602@gmail.com>
2022-08-17 15:44:31 -0300

    Button done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ styles }) =>(
+  <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins front-medium text-[18px] text-primary rounded-[10px] outline-none ${styles}`}>
+    Get Started
+  </button>
+)
 
 export default Button
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
 
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1 '>
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
