3825949f8cb4270a088d11b5dc17698a42b7bb0d
Diogo0602x <diogoteixeira0602@gmail.com>
2022-08-19 14:39:00 -0300

    Clients and Testiomials done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,8 +4,8 @@ import FeedBackCard from './FeedBackCard';
 
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`} >
-    {/* { TODO } */}
-    <div/>
+    <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
+
     <div className='w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
       <h1 className={styles.heading2}>
         What people are <br className='sm:block hidden'/> saying about us
